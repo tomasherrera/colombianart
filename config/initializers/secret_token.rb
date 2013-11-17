cad11b75c3ec5760ad08533726b9c120eb8942d8
Tomas Herrera <tomasherrera91@hotmail.com>
2013-11-16 19:28:28 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colombianart2::Application.config.secret_token = '6de3dd108f7afb1d21d713a08ec9e34d69ff4cc54acb77ca530b6fb3548f3129da593debdac75f787ff8a5ce0cebb8790ea3e57a4473edb0975a89d743058f51'
